bc692b28d853a1777a975806e3cd96f779146c92
mprzepiorkowski <mprzepiorkowski@gmail.com>
2019-05-30 22:14:34 +0200

    removed unused event

--- app/src/main/java/com/machineinsight_it/postviewer/ui/base/event/DataSetChangedEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.machineinsight_it.postviewer.ui.base.event
-
-class DataSetChangedEvent : SingleLiveEvent<Pair<Int, Int>>() {
-    fun call(start: Int, end: Int) {
-        value = Pair(start, end)
-    }
-}
